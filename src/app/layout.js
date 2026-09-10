8fe111aa332ee718b2174884f439fb8e73eeea61
Areg472 <aregazatyank@outlook.com>
2026-09-10 21:42:03 +0400

    fix 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,10 +38,10 @@ export default function RootLayout({ children }) {
         {children}
         <footer className="w-full bg-[#1d1e25] py-4 text-center">
           <a className="hmm" href="https://github.com/re1exe" target="_blank">
-            <p className="credit">Some code by @Re1</p>
+            <p className="credit">Homepage design by @Re1</p>
           </a>
         </footer>
-      <Analytics/>
+        <Analytics />
       </body>
     </html>
   );
